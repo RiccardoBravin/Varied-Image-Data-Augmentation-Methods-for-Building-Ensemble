6786948d3e6731ecae867ccfb8537399d4c6c172
Riccardo Bravin <riccardo.bravin.1@studenti.unipd.it>
2022-04-29 10:21:03 +0200

    results modified

--- results/prof/DatasColor 34/results.m
+++ results/prof/DatasColor 34/results.m
@@ -1,6 +1,6 @@
 clear
-for i = 1:5
-    accuracy{i} = load(strcat("DatasColor_37_", num2str(i), "_accuracy.mat")).accuracy;
+for i = 1:10
+    accuracy{i} = load(strcat("DatasColor_34_", num2str(i), "_accuracy.mat")).accuracy;
 end
 
 %accuracy
@@ -9,9 +9,9 @@ for i = 1:16
     for j = 1:size(accuracy,2)
        sum = sum + accuracy{j}{2,i}; 
     end
-    mean = sum/size(accuracy,2);
-    disp(num2str(mean*100));
-    %disp( strcat(accuracy{1}{1,i}, " = ", num2str(mean*100),"%"));
+    mean = sum/10;
+    %disp(num2str(mean*100));
+    disp( strcat(accuracy{1}{1,i}, " = ", num2str(mean*100),"%"));
 end
 
 %accuracy for class
@@ -20,7 +20,7 @@ for i = 1:16
     for j = 1:size(accuracy,2)
        score = score + accuracy{j}{3,i}; 
     end
-    mean = score./size(accuracy,2);
+    mean = score./10;
     disp(num2str(mean));
     %disp( strcat(accuracy{1}{1,i}, " = ", num2str(mean*100),"%"));
 end
--- results/prof/Paintings/results.m
+++ results/prof/Paintings/results.m
@@ -1,36 +1,37 @@
+
 clear
-for i = 1:5
-    accuracy{i} = load(strcat("DatasColor_37_", num2str(i), "_accuracy.mat")).accuracy;
+for i = 1:10
+    accuracy{i} = load(strcat("paintings_fold", num2str(i), "_accuracy.mat")).accuracy;
 end
 
 %accuracy
-for i = 1:16
+for i = 1:10
     sum = 0;
     for j = 1:size(accuracy,2)
        sum = sum + accuracy{j}{2,i}; 
     end
-    mean = sum/size(accuracy,2);
+    mean = sum/10;
     disp(num2str(mean*100));
     %disp( strcat(accuracy{1}{1,i}, " = ", num2str(mean*100),"%"));
 end
 
 %accuracy for class
-for i = 1:16
+for i = 1:10
     score = 0;
     for j = 1:size(accuracy,2)
        score = score + accuracy{j}{3,i}; 
     end
-    mean = score./size(accuracy,2);
+    mean = score./10;
     disp(num2str(mean));
     %disp( strcat(accuracy{1}{1,i}, " = ", num2str(mean*100),"%"));
 end
 
-
-for i = 1:16
-    mat = 0;
-    for j = 1:size(accuracy,2)
-       mat = mat + accuracy{j}{4,i}; 
-    end
-    confusionchart(mat);
-    pause(1);
-end
+% 
+% for i = 1:16
+%     mat = 0;
+%     for j = 1:size(accuracy,2)
+%        mat = mat + accuracy{j}{4,i}; 
+%     end
+%     confusionchart(mat);
+%     pause(1);
+% end
